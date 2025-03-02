dff7c1fea115ef1da385237ff4f50567a9985e4f
Nikita Rajawat <nikita@gmail.com>
2025-03-02 12:22:18 +0530

    resume updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,25 @@
-// import React, { useState } from 'react'
-// import './Navbar.css'
-// // import underline from '../../assets/under.webp'
-
-// import logo from '../../assets/logo.png'
-// const Navbar = () => {
-
-//   const [menu,setMenu] = useState("home");
-//   return (
-//     <div id='navbar' className='navbar'>
-//         <img src={logo} alt="" />
-//         <ul className='nav-menu'>
-//             <li><p onClick={()=>setMenu("home")}>Home</p></li>
-//             <li><p onClick={()=>setMenu("about")}>About me</p></li>
-//             <li><p onClick={()=>setMenu("services")}>Services</p></li>
-//             <li><p onClick={()=>setMenu("work")}>Portfolio</p></li>
-//             <li><p onClick={()=>setMenu("contact")}>Contact</p></li>
-//         </ul>
-//         <div className='nav-connect'>Connect With Me</div>
-//     </div>
-//   )
-// }
-
-// export default Navbar;
-
-
-
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-
+import { MdOutlineLegendToggle } from 'react-icons/md';
+import { MdClose } from 'react-icons/md';
 const Navbar = () => {
   const [menu, setMenu] = useState('home');
+  const menuRef = useRef();
 
+  const openMenu = () => {
+    menuRef.current.style.right="0";
+  }
+  const closeMenu = () => {
+    menuRef.current.style.right="-350px";
+  }
   return (
     <div id='navbar' className='navbar'>
       <img src={logo} alt="Website Logo" />
-      <ul className='nav-menu'>
+      <MdOutlineLegendToggle onClick={openMenu} className="icon-open"/>
+      <ul ref={menuRef} className='nav-menu'>
+      <MdClose onClick={closeMenu} className="icon-close"/>
         <li className={menu === 'home' ? 'active' : ''}>
           <AnchorLink className='anchor-link' href='#home' ><p onClick={() => setMenu('home')}>Home</p></AnchorLink>
         </li>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,29 +1,3 @@
-// import React from 'react'
-// import './Hero.css'
-// // import profile_img from '../../assets/nidhi-black(1).jpeg'
-// import profile_img from '../../assets/n4.jpeg'
-// import AnchorLink from 'react-anchor-link-smooth-scroll'
-// const Hero = () => {
-//   return (
-//     <div id='hero' className='hero'>
-//         <img src={profile_img} alt="" />
-//         <h1><span>I'm Nidhi Rajawat</span>, frontend developer based in India.</h1>
-//         <p>I specialize in building user-friendly and visually appealing web applications. I am passionate about coding, design, and creating seamless user experiences.</p>
-//         <div className='hero-action'>
-//             <div className='hero-connect'><AnchorLink className='anchor-link' offset={50} href='#contact' >Connect with me</AnchorLink></div>
-//             <div className="hero-resume">My resume</div>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default Hero
-
-
-
-
-
-
 import React from 'react';
 import './Hero.css';
 import profile_img from '../../assets/n4.jpeg';
@@ -49,6 +23,15 @@ const Hero = () => {
             className="resume-link">
             My Resume
           </a>
+
+
+          {/* <a 
+  href="/nidhirajawat.pdf" 
+  download="Nidhi_Rajawat_Resume.pdf" 
+  className="resume-link">
+  My Resume
+</a> */}
+
         </div>
       </div>
     </div>
